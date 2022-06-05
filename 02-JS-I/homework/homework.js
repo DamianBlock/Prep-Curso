ad69e027ac7f51cb82281705036fa726bc1a47e1
DamianBlock <damianfrt25@gmail.com>
2022-06-05 12:20:03 -0300

    update 10

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -194,7 +194,11 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if (mumero === 0) {
+    return false;
+  } else if (numero > 0) {
+          return 'Es positivo';
+  } return 'Es negativo';
 }
 
 function agregarSimboloExclamacion(str) {
