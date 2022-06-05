05da407bf3ad5447ae89c67660ff8721469c46a2
DamianBlock <damianfrt25@gmail.com>
2022-06-05 13:23:25 -0300

    update 11

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -69,6 +69,12 @@ function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  switch (numero) {
+    case 10: return true
+    break;
+    case 5: return false
+    break;
+    }
 }
 
 function estaEnRango(numero) {
