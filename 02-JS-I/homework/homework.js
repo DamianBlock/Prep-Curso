5a54014536fb002ec50cae668e002cee04edb33a
DamianBlock <damianfrt25@gmail.com>
2022-06-22 11:55:08 -0300

    update 11

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,6 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
@@ -17,7 +16,6 @@ const nuevaResta = 10 - 5 === 5;
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-
 const nuevoModulo = 21 % 5 === 1;
 
 
@@ -34,15 +32,12 @@ function devolverString(str) {
   return str;
 }
 
-var x;
-var y;
-
 function suma(x, y) {
 
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
- return x + y;
+  return x + y;
   // return x + y;
   // suma(4,2) debería dar como resultado 6
 }
@@ -68,51 +63,46 @@ function divide(x, y) {
 
    // Divide "x" entre "y" y devuelve el valor
   // Tu código:
- return x/y;
+  return x/y;
   // return x/y;
   
 }
 
 function sonIguales(x, y) {
-
-  if (x === y) {
-     return true;
-                } 
-                else   
-                return false;
-                
-  }
-
-  
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
-
+  if (x === y) {
+    return true;
+               } 
+               else   { 
+                  return false;
+               }
+               
+}
 
 function tienenMismaLongitud(str1, str2) {
-
-
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
   if (str1.length === str2.length)
-      {   
-        return true;
-      }
-                 else  return false;
+  {   
+    return true;
+  }
+             else  return false;
+  
 }
-
-var num;
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num < 90) {
-                return true;
-                }
-                        else return false;
+   // Tu código:
+   if (num < 90) {
+    return true;
+    }
+            else return false;
+  
 }
 
 function mayorQueCincuenta(num) {
@@ -130,24 +120,24 @@ function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
   return x % y;
+
+  
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 === 0) {
-                      return true;
-                      } return false;
+  
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num%2 === 1) {
-                     return true;
-                    } return false;
+  if (num % 2 === 0) {
+    return true;
+    } return false;
 }
 
 function elevarAlCuadrado(num) {
@@ -155,38 +145,42 @@ function elevarAlCuadrado(num) {
   // ojo: No es raiz cuadrada!
   // Tu código:
   return Math.pow(num,2);
+
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
   return Math.pow(num,3);
+
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
   return Math.pow(num,exponent);
+
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
   return Math.round(num);
-  
+
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
   return Math.ceil(num);
-  
+
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
   return Math.random();
+
 }
 
 function esPositivo(numero) {
@@ -206,6 +200,7 @@ function agregarSimboloExclamacion(str) {
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
   return (str+'!');
+
 }
 
 function combinarNombres(nombre, apellido) {
@@ -213,6 +208,7 @@ function combinarNombres(nombre, apellido) {
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
   return nombre+' '+apellido;
+
 }
 
 function obtenerSaludo(nombre) {
@@ -220,12 +216,14 @@ function obtenerSaludo(nombre) {
   // "Martin" -> "Hola Martin!"
   // Tu código:
   return "Hola "+nombre+"!";
+
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
   return alto * ancho;
+
 }
 
 
@@ -233,6 +231,7 @@ function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
   return lado*4;
+
 }
 
 
@@ -240,6 +239,7 @@ function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
   return (base*altura)/2;
+
 }
 
 
@@ -248,6 +248,7 @@ function deEuroAdolar(euro){
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
   return euro * 1.20;
+
 }
 
 
@@ -257,13 +258,13 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
- if (letra.length > 1) { 
-                         "Dato incorrecto";
-                        } 
- if (letra === 'a' || letra === 'A' || letra === 'e' || letra === 'E' || letra === 'i' || letra === 'I' || letra === 'o' || letra === 'O' || letra === 'u' || letra === 'U')
- {
-   return "Es vocal";
- } return "Dato incorrecto";
+  if (letra.length > 1) { 
+    "Dato incorrecto";
+   } 
+if (letra === 'a' || letra === 'A' || letra === 'e' || letra === 'E' || letra === 'i' || letra === 'I' || letra === 'o' || letra === 'O' || letra === 'u' || letra === 'U')
+{
+return "Es vocal";
+} return "Dato incorrecto";
 }
 
 
--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -138,6 +138,12 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
+  for (var i=0 ; i < numero ; i++) {
+    if( numero % 1 === numero && numero % numero === 1) { 
+      return true;
+    }
+    return false;
+  }
 }
 
 function esVerdadero(valor){
